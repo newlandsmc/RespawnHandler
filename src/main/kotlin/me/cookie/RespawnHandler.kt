eb291347e55739f161824037f39ce0df591c7852
Badbird5907 <50347938+Badbird5907@users.noreply.github.com>
2022-12-24 11:07:09 -0500

    oops

--- src/main/kotlin/me/cookie/RespawnHandler.kt
+++ src/main/kotlin/me/cookie/RespawnHandler.kt
@@ -45,7 +45,7 @@ class RespawnHandler : JavaPlugin() {
                     "z int",
                     "world varchar(255)",
                     "cause varchar(255)",
-                    "inventory LARGETEXT",
+                    "inventory varchar(MAX)",
                     "timestamp bigint",
                     "claimed boolean",
                     "items int",
